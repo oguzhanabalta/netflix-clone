e925ee79cfd5dd484167d5db0ee4a3608f883d2e
Oguzhan <oguzhanbalta99@gmail.com>
2022-01-21 01:39:38 +0300

    Accordion Component

---
+++ zugoflix/src/containers/faqs.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Accordion from "../components/accordion";
+import faqsData from '../fixtures/faqs.json';
+
+export function FaqsContainer(){
+    return(
+        <Accordion>
+            <Accordion.Title>Sıkça Sorulan Sorular</Accordion.Title>
+            {faqsData.map(item =>(
+                <Accordion.Item key={item.id}>
+                    <Accordion.Header>{item.header}</Accordion.Header>
+                    <Accordion.Body>{item.body}</Accordion.Body>
+                </Accordion.Item>
+            ))}
+        </Accordion>
+
+    )
+}
