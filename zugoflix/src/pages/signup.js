30a6ce20767db159826a0e915e6d576826c42ca0
Oguzhan <oguzhanbalta99@gmail.com>
2022-01-26 17:33:56 +0300

    SignUp page completed and registration process.

--- zugoflix/src/pages/signup.js
+++ zugoflix/src/pages/signup.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
-export default function Signup(){
-    return(
-        <div></div>
-    )
+export default function SignUp() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
+    const [firstName, setFirstName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const isInvalid = firstName === '' || password === '' || emailAddress === '';
+
+    const handleSignup = (event) => {
+        event.preventDefault();
+
+        return firebase
+            .auth()
+            .createUserWithEmailAndPassword(emailAddress, password)
+            .then((result) =>
+                result.user
+                    .updateProfile({
+                        displayName: firstName,
+                        photoURL: Math.floor(Math.random() * 5) + 1,
+                    })
+                    .then(() => {
+                        history.push(ROUTES.BROWSE);
+                    })
+            )
+            .catch((error) => {
+                setFirstName('');
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            });
+    };
+
+    return (
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign Up</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+
+                    <Form.Base onSubmit={handleSignup} method="POST">
+                        <Form.Input
+                            placeholder="İsim"
+                            value={firstName}
+                            onChange={({ target }) => setFirstName(target.value)}
+                        />
+                        <Form.Input
+                            placeholder="Email adresi"
+                            value={emailAddress}
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input
+                            type="Şifre"
+                            value={password}
+                            autoComplete="off"
+                            placeholder="Password"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-up">
+                            Kayıt Ol
+                        </Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>
+                        Zaten bir kullanıcı hesabınız var mı? <Form.Link to="/signin">Oturum aç.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        Bu sayfa robot olmadığınızı kanıtlamak için Google reCAPTCHA tarafından korunuyor. Daha fazlasını öğrenin.
+                    </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer />
+        </>
+    );
 }
