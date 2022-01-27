0fbeac5c8863e1e6e91df629847da033c653a943
Oguzhan <oguzhanbalta99@gmail.com>
2022-01-28 01:31:26 +0300

    Card Component is completed.

--- zugoflix/src/containers/browse.js
+++ zugoflix/src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
 import { FirebaseContext } from '../context/firebase'
-import { Loading, Header } from "../components";
+import { Loading, Header, Card } from "../components";
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg'
 
@@ -11,6 +11,8 @@ export default function BrowseContainer({ slides }) {
     const [searchTerm, setSearchTerm] = useState('');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
+    const [slideRows,  setSlideRows] = useState([]);
+
     const { firebase } = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
 
@@ -21,6 +23,10 @@ export default function BrowseContainer({ slides }) {
         }, 3000);
     }, [profile.displayName]);
 
+    useEffect(()=>{
+        setSlideRows(slides[category]);
+    }, [slides, category])
+
     return (
         profile.displayName ? (
             <>
@@ -30,10 +36,16 @@ export default function BrowseContainer({ slides }) {
                 <Header.Frame>
                         <Header.Group>
                             <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
-                            <Header.TextLink >
+                            <Header.TextLink 
+                                active={category=== 'series' ? 'true' : 'false'}
+                                onClick={()=> setCategory('series')}            
+                            >
                                 Diziler
                             </Header.TextLink>
-                            <Header.TextLink >
+                            <Header.TextLink 
+                                active={category=== 'films' ? 'true' : 'false'}
+                                onClick={()=> setCategory('films')}   
+                            >
                                 Filmler
                             </Header.TextLink>
                         </Header.Group>
@@ -65,6 +77,33 @@ export default function BrowseContainer({ slides }) {
                         <Header.PlayButton>Oynat</Header.PlayButton>
                     </Header.Feature>
                 </Header>
+
+                <Card.Group>
+                    {slideRows.map((slideItem) =>(
+                        <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                            <Card.Title>{slideItem.title}</Card.Title>
+                            <Card.Entities>
+                                {slideItem.data.map((item)=>(
+                                    <Card.Item key={item.docId} item={item}>
+                                        <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`}/>
+                                        <Card.Meta>
+                                            <Card.SubTitle>{item.title}</Card.SubTitle>
+                                            <Card.Text>{item.description}</Card.Text>
+                                        </Card.Meta>
+                                    </Card.Item>
+                                ))}
+                            </Card.Entities>
+                           <Card.Feature category={category}>
+                                <p>hello</p>
+                                {/* <Player>
+                                    <Player.Button/>
+                                    <Player.Video src="/videos/bunny.mp4    "/>
+                                </Player>  */}
+                            </Card.Feature> 
+                        </Card>
+                    ))}
+
+                </Card.Group>
 
 
 
