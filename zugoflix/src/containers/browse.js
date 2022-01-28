309c03456e39000ef3cdc358cb7d4b49c119b90c
Oguzhan <oguzhanbalta99@gmail.com>
2022-01-28 16:05:38 +0300

    last changes.

--- zugoflix/src/containers/browse.js
+++ zugoflix/src/containers/browse.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { SelectProfileContainer } from "./profiles";
+import Fuse from "fuse.js";
 import { FirebaseContext } from '../context/firebase'
 import { FooterContainer } from "./footer";
 import { Loading, Header, Card, Player } from "../components";
@@ -28,6 +29,20 @@ export default function BrowseContainer({ slides }) {
         setSlideRows(slides[category]);
     }, [slides, category])
 
+
+    useEffect(()=>{
+        const fuse = new Fuse(slideRows, {
+            keys: ['data.description', 'data.title', 'data.genre'],
+
+        });
+        const results = fuse.search(searchTerm).map(({item})=>item);
+
+        if(slideRows.length>0 &&searchTerm.length>3 && results.length>0 ){
+            setSlideRows(results);
+        } else {
+            setSlideRows(slides[category]);
+        }
+    },[searchTerm])
     return (
         profile.displayName ? (
             <>
